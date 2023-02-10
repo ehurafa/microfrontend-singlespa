aea7f6624c905137f7b9a47fcadf749a7df1cbe9
ehurafa <rafarfl2010@gmail.com>
2023-02-10 12:59:54 -0400

    config orcherstrator

---
+++ react-single/webpack.config.js
@@ -0,0 +1,22 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "rg",
+    projectName: "react-single",
+    webpackConfigEnv,
+    argv,
+    devServer: {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+        "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+      }
+    }
+  });
+
+  return merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+  });
+};
